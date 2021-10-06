d893260f79a25d216cf4e68d206eb6546546f384
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-10-06 19:31:53 +0530

    storing data to the localstorage using useEffect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import AddContact from './Components/AddContact';
 import ContactList from './Components/ContactList';
 import Header from './Components/Header';
 
 function App() {
-
+  const LOCAL_STORAGE_KEY = "contacts";
   const[contacts,setContacts]=useState([]);
 
   const addContactHandler = (contact) => {
     console.log(contact);
     setContacts([...contacts, contact])
   }
+
+  useEffect(()=>{
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(retriveContacts) setContacts(retriveContacts);
+}, []);
+
+  useEffect(()=>{
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
+  }, [contacts]);
 
   return (
     <div className="ui container">
