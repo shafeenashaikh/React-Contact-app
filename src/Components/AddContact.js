8b25645ed7c9d28292333089e60309e30289ec6a
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-10-06 12:24:06 +0530

    edited AddContact

--- src/Components/AddContact.js
+++ src/Components/AddContact.js
@@ -3,18 +3,20 @@ import React, { Component } from 'react';
 class AddContact extends Component{
     render(){
         return(
-            <div>
+            <div className="ui main">
+                
                 <h2>Add Contact</h2>
-                <form>
+
+                <form className="ui form">
                     <div className="field">
-                        <label>Name: </label>
+                        <label>Name</label>
                         <input type="text" name="name" placeholder="Name"/>
                     </div><br/>
-                    <div>
-                        <label>Email: </label>
+                    <div className="field">
+                        <label>Email</label>
                         <input type="text" name="email" placeholder="Email"/>
                     </div><br/>
-                    <button style={{backgroundColor:"#3b6ff5", color:"white", padding: "8px 10px", fontSize:"20px"}}>  Add</button>
+                    <button className="ui button blue">Add</button>
                 </form>
             </div>
         )
