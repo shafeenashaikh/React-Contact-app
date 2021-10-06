64433a0c59df326bf530604ab884cda045a99b17
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-10-06 13:02:22 +0530

    created Contact List

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 import AddContact from './Components/AddContact';
+import ContactList from './Components/ContactList';
 import Header from './Components/Header';
 
 function App() {
+
+  const contacts = [
+    {
+      id:"1",
+      name:"john",
+      email:"john@gmail.com"
+    },
+    {
+      id:"2",
+      name:"jai",
+      email:"jai@gmail.com"
+    }
+  ];
+
   return (
     <div className="ui container">
       <Header/>
       <AddContact/>
+      <ContactList contacts={contacts}/>
     </div>
   );
 }
---
+++ src/Components/ContactCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ContactCard = (props) => {
+
+    const{id, name, email} = props.contact;
+    return(
+        <div className="item">
+        <div className="content">
+            <div className="header">{name}</div>
+            <div className="header">{email}</div>
+        </div><br/>
+        <i className="trash alternate outline icon" 
+        style={{color:"red", marginTop:"7px"}}
+        ></i>
+    </div>
+    )
+}
+export default ContactCard
